987bc518e3919f20c2e0e53cdcfe1c54e16f9f73
Timur Iderisov <timer-1988@mail.ru>
2022-07-21 21:50:11 +0300

    fix classes

--- hw04-generics/src/main/java/CustomerService.java
+++ hw04-generics/src/main/java/CustomerService.java
@@ -1,20 +1,28 @@
-import java.util.Map;
+import java.util.*;
 
 public class CustomerService {
 
-    //todo: 3. надо реализовать методы этого класса
-    //важно подобрать подходящую Map-у, посмотрите на редко используемые методы, они тут полезны
+    private Comparator comparator = Comparator.comparingLong(Customer::getScores);
+    private NavigableMap<Customer, String> map = new TreeMap(comparator);
 
     public Map.Entry<Customer, String> getSmallest() {
-        //Возможно, чтобы реализовать этот метод, потребуется посмотреть как Map.Entry сделан в jdk
-        return null; // это "заглушка, чтобы скомилировать"
+        return copy(map.firstEntry());
     }
 
     public Map.Entry<Customer, String> getNext(Customer customer) {
-        return null; // это "заглушка, чтобы скомилировать"
+        return copy(map.higherEntry(customer));
     }
 
     public void add(Customer customer, String data) {
+        map.put(customer, data);
+    }
 
+    private Map.Entry<Customer, String> copy(Map.Entry<Customer, String> entry) {
+        if (entry == null) {
+            return null;
+        }
+
+        Customer original = entry.getKey();
+        return Map.entry(new Customer(original.getId(), original.getName(), original.getScores()), entry.getValue());
     }
 }
--- hw04-generics/src/test/java/CustomerTest.java
+++ hw04-generics/src/test/java/CustomerTest.java
@@ -9,10 +9,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class CustomerTest {
 
-    // Все тесты должны проходить, менять тесты не надо.
-
     @Test
-    @Disabled
     @DisplayName("Проверяем, что класс Customer не сломан")
     void setterCustomerTest() {
         //given
@@ -28,7 +25,6 @@ class CustomerTest {
     }
 
     @Test
-    @Disabled //надо удалить
     @DisplayName("Объект Customer как ключ в карте")
     void customerAsKeyTest() {
         //given
@@ -56,7 +52,6 @@ class CustomerTest {
     }
 
     @Test
-    @Disabled //надо удалить
     @DisplayName("Сортировка по полю score, итерация по возрастанию")
     void scoreSortingTest() {
         //given
@@ -96,7 +91,6 @@ class CustomerTest {
     }
 
     @Test
-    @Disabled //надо удалить
     @DisplayName("Модификация коллекции")
     void mutationTest() {
         //given
@@ -118,7 +112,6 @@ class CustomerTest {
     }
 
     @Test
-    @Disabled //надо удалить
     @DisplayName("Возвращание в обратном порядке")
     void reverseOrderTest() {
         //given
