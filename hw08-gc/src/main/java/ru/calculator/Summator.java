a57c0ee6cc5728ce8cf788e2c3e7d740d17c0adf
Timur Iderisov <timer-1988@mail.ru>
2022-09-03 18:32:57 +0300

    hw08 gc

--- hw08-gc/src/main/java/ru/calculator/Summator.java
+++ hw08-gc/src/main/java/ru/calculator/Summator.java
@@ -1,21 +1,18 @@
 package ru.calculator;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Summator {
-    private Integer sum = 0;
-    private Integer prevValue = 0;
-    private Integer prevPrevValue = 0;
-    private Integer sumLastThreeValues = 0;
-    private Integer someValue = 0;
-    private final List<Data> listValues = new ArrayList<>();
+    private int sum = 0;
+    private int prevValue = 0;
+    private int prevPrevValue = 0;
+    private int sumLastThreeValues = 0;
+    private int someValue = 0;
+    private int size = 0;
 
     //!!! сигнатуру метода менять нельзя
     public void calc(Data data) {
-        listValues.add(data);
-        if (listValues.size() % 6_600_000 == 0) {
-            listValues.clear();
+        size++;
+        if (size % 6_600_000 == 0) {
+            size = 0;
         }
         sum += data.getValue();
 
@@ -25,28 +22,28 @@ public class Summator {
         prevValue = data.getValue();
 
         for (var idx = 0; idx < 3; idx++) {
-            someValue += (sumLastThreeValues * sumLastThreeValues / (data.getValue() + 1) - sum);
-            someValue = Math.abs(someValue) + listValues.size();
+            someValue += (sumLastThreeValues * sumLastThreeValues / (prevValue + 1) - sum);
+            someValue = Math.abs(someValue) + size;
         }
     }
 
-    public Integer getSum() {
+    public int getSum() {
         return sum;
     }
 
-    public Integer getPrevValue() {
+    public int getPrevValue() {
         return prevValue;
     }
 
-    public Integer getPrevPrevValue() {
+    public int getPrevPrevValue() {
         return prevPrevValue;
     }
 
-    public Integer getSumLastThreeValues() {
+    public int getSumLastThreeValues() {
         return sumLastThreeValues;
     }
 
-    public Integer getSomeValue() {
+    public int getSomeValue() {
         return someValue;
     }
 }
