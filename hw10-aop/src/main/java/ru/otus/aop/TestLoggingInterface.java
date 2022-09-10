4dfb98240ad8d98364daac7170420715cd356e9c
Timur Iderisov <timer-1988@mail.ru>
2022-09-10 17:56:29 +0300

    hw10 aop

---
+++ hw10-aop/src/main/java/ru/otus/aop/TestLoggingInterface.java
@@ -0,0 +1,10 @@
+package ru.otus.aop;
+
+public interface TestLoggingInterface {
+
+    public void calculation(int param);
+
+    public void calculation(String param);
+
+    public void calculation(String param, String param2);
+}
